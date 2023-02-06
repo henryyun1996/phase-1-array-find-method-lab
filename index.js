a3598c4291506538dc9362ef77dfa3fc7279ae16
Henry <henrysungyun@gmail.com>
2023-02-05 22:23:45 -0800

    woot

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(superBowlResults) {
+    let win = superBowlResults.find(function(results) {
+        return results.result === "W"; 
+    });
+    if (win) {
+        return win.year;
+    } else {
+        return undefined;
+    }
+}
